347d2779896d36a43756be4562185141f562fede
Preston <prestonosoro56@gmail.com>
2024-10-01 05:58:54 +0300

    feat: create Contact page for client inquiries

---
+++ src/components/pages/client/Contact.tsx
@@ -0,0 +1,12 @@
+import ContactForm from '../../sections/ContactForm';
+
+const Contact = () => {
+  return (
+    <div className="contact py-20">
+      <h1 className="text-4xl text-center">Get in Touch</h1>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default Contact;

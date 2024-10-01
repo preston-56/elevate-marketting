fd3d43fd3bf58382761e7d546c70e3b709b1e4ce
Preston <prestonosoro56@gmail.com>
2024-10-01 05:58:35 +0300

    feat: add NonprofitPackages component for nonprofit packages

---
+++ src/components/package/NonprofitPackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NonprofitPackages: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Nonprofit Packages</h3>
+      <p>Special rates for nonprofit organizations.</p>
+    </div>
+  );
+};
+
+export default NonprofitPackages;

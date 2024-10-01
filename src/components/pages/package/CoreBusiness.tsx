18d8d1a0c059c0e1d667cb8464de5d76908c201e
Preston <prestonosoro56@gmail.com>
2024-10-01 05:59:21 +0300

    feat: add CoreBusiness page for business package details

---
+++ src/components/pages/package/CoreBusiness.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoreBusinessPackages from '../../package/CoreBusinessPackages';
+
+const CoreBusiness: React.FC = () => {
+  return (
+    <div>
+      <CoreBusinessPackages />
+    </div>
+  );
+};
+
+export default CoreBusiness;

829bd47b10aa89365fd1f723d856e1cd5064a078
Preston <prestonosoro56@gmail.com>
2024-10-01 05:54:24 +0300

    feat: add basic feature for artist packages

---
+++ src/components/package/ArtistPackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ArtistPackages: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Artist Packages</h3>
+      <p>Choose from Basic, Pro, and Premium packages.</p>
+    </div>
+  );
+};
+
+export default ArtistPackages;

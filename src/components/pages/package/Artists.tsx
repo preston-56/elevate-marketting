8b19a701703c3d32e731c041febe3b3da94a3ed5
Preston <prestonosoro56@gmail.com>
2024-10-01 05:49:16 +0300

    chore: import artist packages

---
+++ src/components/pages/package/Artists.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArtistPackages from '../../package/ArtistPackages';
+
+const Artists: React.FC = () => {
+  return (
+    <div>
+      <ArtistPackages />
+    </div>
+  );
+};
+
+export default Artists;

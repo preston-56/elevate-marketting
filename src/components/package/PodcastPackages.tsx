1c9f30750ecdc22f1e0ccb092174a672c01a930e
Preston <prestonosoro56@gmail.com>
2024-10-01 05:58:41 +0300

    feat: add PodcastPackages component for podcast packages

---
+++ src/components/package/PodcastPackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PodcastPackages: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Podcast Packages</h3>
+      <p>Choose from Basic, Pro, and Premium packages.</p>
+    </div>
+  );
+};
+
+export default PodcastPackages;

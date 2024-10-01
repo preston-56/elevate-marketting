ad589770c7c7e000ffafaa2e8610fd8148851d13
Preston <prestonosoro56@gmail.com>
2024-10-01 05:58:28 +0300

    feat: add FinancialPackages component for financial packages

---
+++ src/components/package/FinancialPackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FinancialPackages: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Financial Packages</h3>
+      <p>Tailored packages for financial services.</p>
+    </div>
+  );
+};
+
+export default FinancialPackages;

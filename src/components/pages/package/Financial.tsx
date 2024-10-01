249b3d5bf1eb42b902b1a8bc238718e34e2425de
Preston <prestonosoro56@gmail.com>
2024-10-01 05:59:28 +0300

    feat: add Financial page for financial package details

---
+++ src/components/pages/package/Financial.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialPackages from '../../package/FinancialPackages';
+
+const Financial: React.FC = () => {
+  return (
+    <div>
+      <FinancialPackages />
+    </div>
+  );
+};
+
+export default Financial;

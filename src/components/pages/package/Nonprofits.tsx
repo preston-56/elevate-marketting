89a80084e57c727a6f1e9225f18e8d980d46c1f2
Preston <prestonosoro56@gmail.com>
2024-10-01 05:59:34 +0300

    feat: add Nonprofits page for nonprofit package details

---
+++ src/components/pages/package/Nonprofits.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NonprofitPackages from '../../package/NonprofitPackages';
+
+const Nonprofits: React.FC = () => {
+  return (
+    <div>
+      <NonprofitPackages />
+    </div>
+  );
+};
+
+export default Nonprofits;

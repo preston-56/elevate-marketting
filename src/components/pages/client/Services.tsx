66f747f01075caaf1c8c3b83e9ba101d95ea1d7b
Preston <prestonosoro56@gmail.com>
2024-10-01 05:59:15 +0300

    feat: create Services page detailing offered services

---
+++ src/components/pages/client/Services.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServicesSection from '../../sections/ServicesSection';
+
+const Services: React.FC = () => {
+  return (
+    <div>
+      <ServicesSection />
+    </div>
+  );
+};
+
+export default Services;

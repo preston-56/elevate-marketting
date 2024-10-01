6f3ea7ae5ccf3ddfaa9031d5986170a54c4d942b
Preston <prestonosoro56@gmail.com>
2024-10-01 05:58:21 +0300

    feat: add CoreBusinessPackages component for business packages

---
+++ src/components/package/CoreBusinessPackages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoreBusinessPackages: React.FC = () => {
+  return (
+    <div>
+      <h3 className="text-xl font-semibold">Core Business Packages</h3>
+      <p>Fundamental packages to elevate your business.</p>
+    </div>
+  );
+};
+
+export default CoreBusinessPackages;

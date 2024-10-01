2a1f05a673c05ddd2fd84c3e8f7481b370afa59f
Preston <prestonosoro56@gmail.com>
2024-10-01 05:59:39 +0300

    feat: add Podcasts page for podcast package details

---
+++ src/components/pages/package/Podcasts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PodcastPackages from '../../package/PodcastPackages';
+
+const Podcasts: React.FC = () => {
+  return (
+    <div>
+      <PodcastPackages />
+    </div>
+  );
+};
+
+export default Podcasts;
